28f782a6745bcae9f7042a27aea7583ed71be74f
l0r3zz <l0r3zz@stublux>
2013-01-03 17:43:50 -0800

    clean up errors injiralab, need to check all other tools to make sure they don't use args.xxx and use auth.xxx

--- jiralab/jiralab.py
+++ jiralab/jiralab.py
@@ -12,8 +12,8 @@ import getpass
 import aes
 import sys
 import os
-#import pexpect
-#import pxssh
+import pexpect
+import pxssh
 
 
 __all__ = []
@@ -21,19 +21,23 @@ __version__ = 0.5
 __date__ = '2012-11-04'
 __updated__ = '2013-01-2'
 
+AES_BLOCKSIZE = 128
+
 # Specialized Exceptions
 class JIRALAB_CLI_TypeError(TypeError): pass
 class JIRALAB_CLI_ValueError(ValueError): pass
 class JIRALAB_AUTH_ValueError(ValueError): pass
+
+
 class Auth():
     """
     Gather user name and password information from either a dict (the dict from argparse works fine)
     """
     def __init__(self,args):
-        AES_BLOCKSIZE = 128
+
         self._salt = "c0ffee31337bea75"
         self.vault_file = ".jiralab_vault-%s"
-        
+
         self.user = args.user
         self.password = args.password
 
@@ -44,16 +48,15 @@ class Auth():
             user = raw_input("Username [%s]: " % getpass.getuser())
             if not user:
                 self.user = getpass.getuser()
-                args.user = self.user
-            args.user = user
+                self.user = self.user
             self.user = user
-            
+
         # set up some paths to search for the vault file
         pass_vault_path =   [
                             ("~%s/" % self.user) + (self.vault_file % self.user),
                             "./" + (self.vault_file % self.user),
                             ]
-                           
+
         # Execute this block if a password was not provided as an argument
         if (not self.password):
             # Iterate through a list of the paths, precedence set by position
@@ -61,23 +64,21 @@ class Auth():
                 if os.path.isfile(path)and os.access(path, os.R_OK):
                     for line in open(path,'r'):
                         self.password = aes.decrypt(line.rstrip('\n'),self._salt,AES_BLOCKSIZE)
-                        args.password = self.password
-                    return # username and password set
+                    return  # username and password set
             # We looked everywhere for the password vault and could not find it, so ask for password
             self.password = getpass.getpass()
-            args.password = self.password
 
-        for path in pass_vault_path : 
+        for path in pass_vault_path :
             try:
-                pwf = open(path,'w')
+                pwf = open(path, 'w')
             except IOError:
                 continue
-            pwf.write(aes.encrypt(self.password,self._salt,AES_BLOCKSIZE))
+            pwf.write(aes.encrypt(self.password, self._salt, AES_BLOCKSIZE))
             pwf.close()
             return
         raise JIRALAB_AUTH_ValueError("Can't write vault file")
 
-            
+
 class CliHelper:
     '''Helper class to do  CLI login, command stream execution
     and file transfers '''
@@ -87,9 +88,8 @@ class CliHelper:
         self.port = port
         self.debug = debug
         self.session = pxssh.pxssh()
-  
 
-    def login(self, user="admin", password="admin", prompt="[#$]",timeout=30):
+    def login(self, user="admin", password="admin", prompt="[#$]", timeout=30):
         self.user = user
         self.password = password
         self.timeout = timeout
@@ -99,7 +99,7 @@ class CliHelper:
                                login_timeout=10, port=self.port,
                                auto_prompt_reset=False)
             #self.session.prompt()
-            
+
         except pxssh.ExceptionPxssh, e:
             self.login = False
             raise(e)
@@ -112,34 +112,33 @@ class CliHelper:
 
     def logout(self):
             self.session.logout()
-    
+
     def _consume_prompt(self):
         # consume the prompt (this is done a lot)
         self.before = self.session.before
         self.after = self.session.after
         self.session.prompt(timeout=1)
 
-
-    def set_prompt(self,prompt):
+    def set_prompt(self, prompt):
         rval = self.PROMPT
         self.PROMPT = self.session.PROMPT = prompt
         return rval
-    
-    def docmd(self, cmd, match,notimeout=False,consumeprompt=True,timeout=30):
+
+    def docmd(self, cmd, match, notimeout=False, consumeprompt=True, timeout=30):
             search_list = match
 
             if not len(match):
                 self.session.sendline(cmd)
                 self._consume_prompt()
                 return 1
-            
+
             if not notimeout:
-                search_list.insert(0,pexpect.TIMEOUT)
-                
+                search_list.insert(0, pexpect.TIMEOUT)
+
             self.session.sendline(cmd)
             rval = self.session.expect(search_list, timeout)
             self.before = self.session.before
             self.after = self.session.after
             if consumeprompt:
                 self._consume_prompt()
-            return rval 
+            return rval
