4469b7138e6221339b017ab6ebbee0baff41d813
Geoff White <geoffw@durganetworks.com>
2013-01-13 22:47:40 -0800

    update to jpass and new jassign

--- jiralab/jassign.py
+++ jiralab/jassign.py
@@ -1,47 +1,47 @@
 #!/usr/bin/python
 # encoding: utf-8
 '''
-jpass -- setup the password vault for a user
-
-
+jassign -- Open and assign a ticket through JIRA
 @author:     geowhite
-@copyright:  2012 StubHub. All rights reserved.
+        
+@copyright:  2013 StubHub. All rights reserved.
+        
 @license:    Apache License 2.0
+
 @contact:    geowhite@stubhub.com
+
 '''
 
 import sys
 import os
 from jira.client import JIRA
 import jiralab
+import json
+
 
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 from argparse import REMAINDER
-
 __all__ = []
 __version__ = 0.1
-__date__ = '2013-01-02'
-__updated__ = '2013-01-2'
-
+__date__ = '2013-01-13'
+__updated__ = '2013-01-13'
 
 TESTRUN = 0
-
 
 class CLIError(Exception):
     '''Generic exception to raise and log different fatal errors.'''
     def __init__(self, msg):
         super(CLIError).__init__(type(self))
         self.msg = "E: %s" % msg
-
     def __str__(self):
         return self.msg
-
     def __unicode__(self):
         return self.msg
 
 
-def main(argv=None):  # IGNORE:C0111
+
+def main(argv=None): # IGNORE:C0111
     '''Command line options.'''
     DEBUG = 0
     if argv is None:
@@ -52,54 +52,67 @@ def main(argv=None):  # IGNORE:C0111
     program_name = os.path.basename(sys.argv[0])
     program_version = "v%s" % __version__
     program_build_date = str(__updated__)
-    program_version_message = '%%(prog)s %s (%s)' % (program_version,
-            program_build_date)
+    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
     program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
 
     try:
         # Setup argument parser
-        parser = ArgumentParser(description=program_shortdesc,
+        parser = ArgumentParser(description=program_shortdesc, 
             formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("-u", "--user", dest="user",
-            default=None, help="user to access JIRA")
-        parser.add_argument("-p", "--password", dest="password",
-            default=None, help="password to access JIRA")
-        parser.add_argument('-d', '--delete', dest="delete", action='count', default=0,
-            help="remove vault file from current working directory")
+        parser.add_argument("-u", "--user", dest="user", default=None,
+            help="user to access JIRA")
+        parser.add_argument("-p", "--password", dest="password", default=None,
+            help="password to access JIRA")
+        parser.add_argument("-a", "--assignee", dest="assignee",
+            help="person to assign the ticket to" )
+        parser.add_argument("-w", "--watchers", dest="watchers",
+            help="comma separated list of watchers" )
+        parser.add_argument("-t", "--text", dest="text",
+            help='"text" (in quotes) to add to the title field')
         parser.add_argument('-v', '--version', action='version',
             version=program_version_message)
         parser.add_argument('-D', '--debug', dest="debug",
             action='store_true', help="turn on DEBUG switch")
-
+        parser.add_argument('rem',
+            nargs=REMAINDER, help="rest of the command goes to the description field")
 
         # Process arguments
         if len(sys.argv) == 1:
             parser.print_help()
             exit(1)
-
+            
         args = parser.parse_args()
-
+            
+             
         if args.debug:
             DEBUG = True
-        # Get username and password from the token file or if one doesn't
-        # exist. Create one.
+                   
         auth = jiralab.Auth(args)
-        if args.delete :
-            try:
-                os.remove("./" + auth.vault_file)
-            except IOError:
-                print( "%WARNING: no vault file found in cwd\n")
-            if args.delete > 1:
-                try:
-                    os.remove(("~%s/" % auth.user) + auth.vault_file)
-                except IOError:
-                    print( '%WARNING: no vault file found in $HOME directory\n')
+        auth.getcred()
+        jira_options = { 'server': 'https://jira.stubcorp.dev/' }
+        jira = JIRA(jira_options,basic_auth= (auth.user,auth.password))
+        if DEBUG: 
+            print( "Creating ticket for  %s " % args.text)
+
         
-        auth.cred()
+        # Create the TOOLS ticket
+        tools_dict = {
+                        'project': {'key':'TOOLS'},
+                        'issuetype': {'name':'Task'},
+                        'assignee': {'name': args.assignee},
+                        'components': [{'name':'decommission'},{'name':'tokenization'}],
+                        'summary': args.test,
+                        'description': args.rem,
+                        }       
+        new_tools = jira.create_issue(fields=tools_dict)
+        
+
+        
 
     except KeyboardInterrupt:
         ### handle keyboard interrupt ###
         return 0
+    
     except Exception, e:
         if DEBUG or TESTRUN:
             raise(e)
@@ -107,7 +120,7 @@ def main(argv=None):  # IGNORE:C0111
         sys.stderr.write(program_name + ": " + str(e) + "\n")
         sys.stderr.write(indent + "  for help use --help\n")
         return 2
-
+    
 
 if __name__ == "__main__":
 
