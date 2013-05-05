ce284887442fae3905e9799fb2e0becace922828
Geoff White <geoffw@durganetworks.com>
2013-05-04 22:55:22 -0700

    eom v 1.081

--- eom/jiralab.py
+++ eom/jiralab.py
@@ -21,9 +21,9 @@ import threading
 
 
 __all__ = []
-__version__ = 0.85
+__version__ = 0.86
 __date__ = '2012-11-04'
-__updated__ = '2013-05-01'
+__updated__ = '2013-05-04'
 
 AES_BLOCKSIZE = 128
 REGSERVER = "srwd00reg010.stubcorp.dev"
