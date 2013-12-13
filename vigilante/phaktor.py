320521b6d714a4165da9c5630036ecc0f5644e04
l0r3zz <l0r3zz@stublux>
2013-12-13 09:41:31 -0800

    create a dict and print that

--- vigilante/phaktor.py
+++ vigilante/phaktor.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python
+# encoding: utf-8
+'''
+phaktor - write facter facts to a FS database
+
+@author:     Geoff White
+@copyright:  2013 StubHub. All rights reserved.
+@license:    Apache License 2.0
+@contact:    geowhite@stubhub.com
+'''
 import os
 import re
 import time
 
 __all__ = []
-__version__ = 0.1
+__version__ = 0.2
 __date__ = '2012-12-11'
-__updated__ = '2013-12-11'
+__updated__ = '2013-12-13'
 
-iso_time = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
 q = open("facts.ftr")
 facts = {}
+event = {}
 for symbol in q.readlines():
     facts[ symbol.rstrip()] = 1
 p = os.popen("facter","r")
-print("{"),
-print('"iso_time :  "%s"' % iso_time),
+iso_time = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
+event["iso_time"] = iso_time
 for line in p.readlines():
     fsp = re.search("(?P<key>.+)=>(?P<value>.+)",line.rstrip())
     if (line.split())[0] in facts:
-        print('"%s" : "%s",' % (fsp.group("key").rstrip(),fsp.group("value").rstrip())),
+        event[fsp.group("key").rstrip()] = fsp.group("value").rstrip()
+print("{"),
+for key, value in event.iteritems():
+    print( '"%s" : "%s",' % (key, value)),
 print("}")
