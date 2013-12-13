1bcfa5d9a7489c4a7661e5b0ea6e565b9473e5cd
l0r3zz <l0r3zz@stublux>
2013-12-13 10:39:19 -0800

    added optparse for -c and -r , create filename for FS storage

--- vigilante/phaktor.py
+++ vigilante/phaktor.py
@@ -11,19 +11,37 @@ phaktor - write facter facts to a FS database
 import os
 import re
 import time
+import socket
+from optparse import OptionParser
 
 __all__ = []
-__version__ = 0.2
+__version__ = 0.3
 __date__ = '2012-12-11'
 __updated__ = '2013-12-13'
 
-q = open("facts.ftr")
+parser = OptionParser()
+parser.add_option("-c", "--config", dest="config_file", default="facts.ftr",
+                  help="file containing keys to save", metavar="FILE")
+parser.add_option("-r","--root", dest="root_dir", default=None,
+                  help="root directory to store events")
+
+(options, args) = parser.parse_args()
+
+q = open(options.config_file)
+
 facts = {}
 event = {}
+
 for symbol in q.readlines():
     facts[ symbol.rstrip()] = 1
+    
+gmtime = time.gmtime()
+iso_time = time.strftime("%Y-%m-%dT%H:%M:%S", gmtime)
+fname_time = time.strftime(".%Y%m%dT%H%M%S", gmtime)
+fname = socket.gethostname() + fname_time
+
 p = os.popen("facter","r")
-iso_time = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
+
 event["iso_time"] = iso_time
 for line in p.readlines():
     fsp = re.search("(?P<key>.+)=>(?P<value>.+)",line.rstrip())
