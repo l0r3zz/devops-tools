a6cd17e691f888ac6a355fe6e565447726b912c6
l0r3zz <l0r3zz@stublux>
2014-01-08 15:56:12 -0800

    Start of vigilante puppet module

---
+++ vigilante/puppet/modules/stubhub-vigilante/manifests/init.pp
@@ -0,0 +1,32 @@
+class stubhub-vigilante {
+  File {
+    owner => root,
+    group => root,
+    mode => 644,
+  }
+
+  file { '/root/learning.answers':
+    ensure => file,
+    source => 'puppet:///modules/learning/learning.answers',
+  }
+
+# Print this info when we log in, too.
+  file {'/etc/motd':
+    ensure => file,
+    owner => root,
+    mode => 0644,
+    content => "Welcome to the Learning Puppet VM! To learn how to write Puppet code, go to
+http://docs.puppetlabs.com/learning and follow along.
+
+To view your current IP address, run `facter ipaddress_eth0`
+
+To log in to the Puppet Enterprise console, go to:
+https://<YOUR IP ADDRESS HERE>
+
+User: puppet@example.com
+Password: learningpuppet
+",
+  }
+
+
+}
