c10f4d971c67f26e73ecd2628c8534ca897c4087
l0r3zz <l0r3zz@stublux>
2014-04-28 17:14:30 -0700

    init.pp test clean with puppet-lint except for line too long and class not documented

--- vigilante/puppet/modules/stubhub-vigilante/manifests/init.pp
+++ vigilante/puppet/modules/stubhub-vigilante/manifests/init.pp
@@ -1,38 +1,44 @@
-class stubhub-vigilante {
+class stubhub-vigilante (
+    $phaktordepth = '10',
+    $phaktor_exe = '/usr/local/bin/phaktor.py',
+    $phaktor_cfg = '/nas/reg/log/jiralab/vigilante/facts.ftr',
+    $phaktor_dir = '/nas/reg/log/jiralab/vigilante/auditor',
+    $phaktor_sched_hour = [ fqdn_rand(4), 6+fqdn_rand(5), 12+fqdn_rand(5), 18+fqdn_rand(5) ],
+){
+
   File {
     owner => root,
     group => root,
-    mode => 644,
-  }
-  
-  
- file { '/usr/local/bin/phaktor.py':
+    mode  => 0644,
+    }
+
+  file { $phaktor_exe :
     ensure => file,
-    mode => 755,
+    mode   => '0755',
     source => 'puppet:///modules/stubhub-vigilante/bin/phaktor.py',
   }
-  
+
   file { '/usr/local/bin/phaktor':
     ensure => link,
-    target => '/usr/local/bin/phaktor.py',
+    target => $phaktor_exe,
   }
 
-  file { '/nas/reg/log/jiralab/vigilante/facts.ftr':
+  file { $phaktor_cfg :
     ensure => file,
     source => 'puppet:///modules/stubhub-vigilante/etc/facts.ftr',
   }
 
-  file { '/nas/reg/log/jiralab/vigilante/auditor':
+  file { $phaktor_dir :
     ensure => directory,
   }
- 
- # NOTE: $phaktordepth needs to be set to the number of audits to keep 
-  cron { "phaktor":
-	user  => root,
-	minute => fqdn_rand(60),
-	hour => [0, 6, 12, 18 ]
-    command => "phaktor -c /nas/reg/log/jiralab/vigilante/facts.ftr -r /nas/reg/log/jiralab/vigilante/auditor -d $phaktordepth",  
-    require => File[ '/nas/reg/log/jiralab/vigilante/facts.ftr'],
+
+
+  cron { 'phaktor':
+    user    => root,
+    minute  => fqdn_rand(60),
+    hour    => $phaktor_sched_hour,
+    command => "phaktor -c ${phaktor_cfg} -r ${phaktor_dir} -d ${phaktordepth}",
+    require => File[ $phaktor_cfg, $phaktor_cfg ],
 
   }
 
