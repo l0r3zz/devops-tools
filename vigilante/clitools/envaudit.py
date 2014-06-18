3aea89a9efa35227bfe2451edc573cc9329e1ab2
imapollo <ze.apollo@gmail.com>
2014-06-18 03:52:12 +0000

    List the current role factor.

--- vigilante/clitools/envaudit.py
+++ vigilante/clitools/envaudit.py
@@ -3,14 +3,6 @@
 '''
 env-audit -- cli tool to perform auditing operations on Dev/QA collection data
 
-jclose is a CLI based tool that allows you to close a JIRA issue from any
-         CLI that can access the JIRA server.  All that is required is your
-         JIRA login and the Issue ID. This program uses jiralab.auth so it
-         will cache your password in an encrypted file so that you don't have
-         to keep entering it, for subsequent commands. If you pipe input into
-         stdin, this input will show up in the comment field of the issue
-         between two {code} tags.
-
 @author:     geowhite
 @copyright:  2014 StubHub. All rights reserved.
 @license:    Apache License 2.0
@@ -19,6 +11,7 @@ jclose is a CLI based tool that allows you to close a JIRA issue from any
 
 import sys
 import os
+import urllib2
 
 
 from argparse import ArgumentParser
@@ -28,8 +21,24 @@ from argparse import REMAINDER
 __all__ = []
 __version__ = 0.1
 __date__ = '2014-06-09'
-__updated__ = '2014-06-09'
+__updated__ = '2014-06-18'
 
+def http_get( request ):
+    return urllib2.urlopen( request ).read()
+
+def api_request( query_type, query_dict ):
+    BASE_URL = 'http://srwd00dvo002.stubcorp.dev:7000/vigilante/api/v0.1'
+    request_url = ''
+    if ( query_type == "collector" ):
+        if "fqdn" in query_dict:
+            request_url = "collector/role/current/%s" % query_dict[ "fqdn" ]
+    else:
+        pass
+
+    if request_url == '':
+        return ''
+
+    return http_get( "%s/%s" % ( BASE_URL, request_url ) )
 
 def main(argv=None):  # IGNORE:C0111
     '''Command line options.'''
@@ -75,14 +84,16 @@ def main(argv=None):  # IGNORE:C0111
     except KeyboardInterrupt:
         ### handle keyboard interrupt ###
         return 0
-#    except Exception, e:
-#        if DEBUG :
-#            raise(e)
-#        indent = len(program_name) * " "
-#        sys.stderr.write(program_name + ": " + str(e) + "\n")
-#        sys.stderr.write(indent + "  for help use --help\n")
-#        return 2
+    except Exception, e:
+        if DEBUG :
+            raise(e)
+        indent = len(program_name) * " "
+        sys.stderr.write(program_name + ": " + str(e) + "\n")
+        sys.stderr.write(indent + "  for help use --help\n")
+        return 2
 
+    if args.list:
+        print api_request( "collector", { "fqdn" : args.role } )
 
 if __name__ == "__main__":
     main()
