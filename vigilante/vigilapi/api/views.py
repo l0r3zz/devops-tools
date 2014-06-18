2e119bce41fca3eec3c5da284d85912aa9742f8e
imapollo <ze.apollo@gmail.com>
2014-06-18 02:51:52 +0000

    Add templates api endpoint.

--- vigilante/vigilapi/api/views.py
+++ vigilante/vigilapi/api/views.py
@@ -7,6 +7,7 @@ from dbapi import VigDB
 
 s = VigDB()
 collector = s.login()
+templates = s.login("template_library")
 
 # Create your views here.
 
@@ -26,4 +27,12 @@ def collector_env_current( request, version, envid ):
 
 def collector_env_time( request, version, starttime, endtime, envid ):
     rs = s.find(collector, {"domain" : envid, "iso8601" : { "starttime" : starttime, "endtime" : endtime } } )
+    return HttpResponse( json.dumps( rs ), content_type='application/json' )
+
+def templates_list( request, version ):
+    rs = s.find(templates, {})
+    return HttpResponse( json.dumps( rs ), content_type='application/json' )
+
+def templates_get( request, version, template_name ):
+    rs = s.find_one(templates, {"name" : template_name})
     return HttpResponse( json.dumps( rs ), content_type='application/json' )
