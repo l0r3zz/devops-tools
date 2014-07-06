41de04bd7b650d8d12619f331ab4e8e69c4f3862
Geoff White <geoffw@durganetworks.com>
2014-07-05 21:27:39 -0700

    pep8 clean up

--- vigilante/clitools/envaudit.py
+++ vigilante/clitools/envaudit.py
@@ -20,28 +20,32 @@ from argparse import RawDescriptionHelpFormatter
 from argparse import REMAINDER
 
 __all__ = []
-__version__ = 0.5
+__version__ = 0.6
 __date__ = '2014-06-09'
-__updated__ = '2014-07-01'
+__updated__ = '2014-07-05'
 
-def http_get( request ):
-    return urllib2.urlopen( request ).read()
 
-def api_request( query_type, query_dict ):
+def http_get(request):
+    return urllib2.urlopen(request).read()
+
+
+def api_request(query_type, query_dict):
     BASE_URL = 'http://srwd00dvo002.stubcorp.dev:7000/vigilante/api/v0.1'
     request_url = ''
-    if ( query_type == "collector" ):
+    if (query_type == "collector"):
         if "fqdn" in query_dict:
-            request_url = "collector/role/current/%s" % query_dict[ "fqdn" ]
+            request_url = "collector/role/current/%s" % query_dict["fqdn"]
         elif "domain" in query_dict:
-            request_url = "collector/env/current/%s" % query_dict[ "domain" ]
-    elif ( query_type == "query" ):
+            request_url = "collector/env/current/%s" % query_dict["domain"]
+    elif (query_type == "query"):
         if "fqdn" in query_dict and "template" in query_dict:
-            request_url = "query/template/%s/collector/role/current/%s" % ( query_dict["template"], query_dict["fqdn"] )
+            request_url = "query/template/%s/collector/role/current/%s" % (
+                query_dict["template"], query_dict["fqdn"])
         if "domain" in query_dict and "template" in query_dict:
-            request_url = "query/template/%s/collector/env/current/%s" % ( query_dict["template"], query_dict["domain"] )
-    elif ( query_type == "templates" ):
-        request_url = "templates/get/%s" %  query_dict["template"] 
+            request_url = "query/template/%s/collector/env/current/%s" % (
+                query_dict["template"], query_dict["domain"])
+    elif (query_type == "templates"):
+        request_url = "templates/get/%s" % query_dict["template"]
         pass
     else:
         pass
@@ -49,7 +53,8 @@ def api_request( query_type, query_dict ):
     if request_url == '':
         return ''
 
-    return http_get( "%s/%s" % ( BASE_URL, request_url ) )
+    return http_get("%s/%s" % (BASE_URL, request_url))
+
 
 def pretty_print_audit(template_struct, result_struct, args):
     summary_msg_role = '\nRole "%s" is %s compliant with template "%s" '
@@ -61,29 +66,34 @@ def pretty_print_audit(template_struct, result_struct, args):
     column_msg = '''
     %s            %s                %s              %s
     '''
-    if result_struct['meta']['type'] == "role-diff" :
+    if result_struct['meta']['type'] == "role-diff":
         compliantP = "NOT" if result_struct['body'] else ""
-        print (summary_msg_role % (args.role, compliantP, template_struct['meta']['name']))
-        if compliantP :
+        print (summary_msg_role %
+              (args.role, compliantP, template_struct['meta']['name']))
+        if compliantP:
             print (header_msg),
-            for key in result_struct['body'] :
-                print(column_msg % (args.role, key, template_struct['body'][key], result_struct['body'][key])),
-    elif result_struct['meta']['type'] == "env-diff" :
+            for key in result_struct['body']:
+                print(column_msg %
+                     (args.role, key, template_struct['body'][key],
+                         result_struct['body'][key])),
+    elif result_struct['meta']['type'] == "env-diff":
         compliantP = ""
-        for role_key in result_struct['body'] :
-            if result_struct['body'][role_key] :
-                if result_struct['body'][role_key][0]['body'] :
+        for role_key in result_struct['body']:
+            if result_struct['body'][role_key]:
+                if result_struct['body'][role_key][0]['body']:
                     compliantP = "NOT"
-        print (summary_msg_env % (args.envid, compliantP, template_struct['meta']['name']))
-        print( header_msg),
-        for role_key in result_struct['body'] :
-            if result_struct['body'][role_key] :
-                if result_struct['body'][role_key][0]['body'] :
-                    for key in result_struct['body'][role_key][0]['body'] :
-                        print(column_msg % (role_key, key, result_struct['template'][role_key]['body'][key], result_struct['body'][role_key][0]['body'][key])),
-            
-            
-                
+        print (summary_msg_env %
+              (args.envid, compliantP, template_struct['meta']['name']))
+        print(header_msg),
+        for role_key in result_struct['body']:
+            if result_struct['body'][role_key]:
+                if result_struct['body'][role_key][0]['body']:
+                    for key in result_struct['body'][role_key][0]['body']:
+                        print(column_msg %
+                            (role_key, key,
+                                result_struct['template'][role_key]['body'][key],
+                                result_struct['body'][role_key][0]['body'][key])),
+
 
 def main(argv=None):  # IGNORE:C0111
     '''Command line options.'''
@@ -98,7 +108,8 @@ def main(argv=None):  # IGNORE:C0111
     program_build_date = str(__updated__)
     program_version_message = '%%(prog)s %s (%s)' % (program_version,
             program_build_date)
-    program_shortdesc ="env-audit - get audit information for Dev/QA environments"
+    program_shortdesc = (
+        "env-audit - get audit information for Dev/QA environments")
 
     try:
         # Setup argument parser
@@ -110,12 +121,13 @@ def main(argv=None):  # IGNORE:C0111
             default=None, help="template to use for auditing")
         parser.add_argument("-r", "--role", dest="role",
             default=None, help="fqdn of the role  to be used ")
-        parser.add_argument('-l', '--list', action='store_true', help="list the template or collection data ",
-            dest="list" )
+        parser.add_argument('-l', '--list', action='store_true',
+		help="list the template or collection data ", dest="list")
         parser.add_argument("-m", "--mgrmode", dest="mm",
-            action='store_true', help="print the information in a more human readable form")
-        parser.add_argument('-v', '--version', action='version', help="print the version ",
-            version=program_version_message)
+            action='store_true',
+		help="print the information in a more human readable form")
+        parser.add_argument('-v', '--version', action='version',
+		help="print the version ", version=program_version_message)
         parser.add_argument('-D', '--debug', dest="debug",
             action='store_true', help="turn on DEBUG switch")
 
@@ -129,48 +141,59 @@ def main(argv=None):  # IGNORE:C0111
         if args.debug:
             DEBUG = True
 
-        if args.list :
-            if args.role :
-                rs =  json.loads(api_request( "collector", { "fqdn" : args.role } ))
-                if args.mm :
+        if args.list:
+            if args.role:
+                rs = json.loads(api_request("collector",
+                                {"fqdn": args.role}))
+                if args.mm:
                     pass
-                else :
-                    print( json.dumps(rs, indent=4, sort_keys=True))
-            elif args.envid :
-                rs =  json.loads(api_request( "collector", { "domain" : args.envid } ))
-                if args.mm :
+                else:
+                    print(json.dumps(rs, indent=4, sort_keys=True))
+            elif args.envid:
+                rs = json.loads(
+                    api_request("collector", {"domain": args.envid}))
+                if args.mm:
                     pass
-                else :
-                    print( json.dumps(rs, indent=4, sort_keys=True))
-            elif args.template :
-                rs =  json.loads(api_request( "templates", { "template" : args.template } ))
-                if args.mm :
+                else:
+                    print(json.dumps(rs, indent=4, sort_keys=True))
+            elif args.template:
+                rs = json.loads(
+                    api_request("templates",
+                                {"template": args.template}))
+                if args.mm:
                     pass
-                else :
-                    print( json.dumps(rs, indent=4, sort_keys=True))
-                
-        elif args.template :
-            tplstruct =  json.loads(api_request( "templates", { "template" : args.template } ))
-            if args.role :
-                rs =  json.loads(api_request( "query", { "fqdn" : args.role, "template" : args.template } ))
+                else:
+                    print(json.dumps(rs, indent=4, sort_keys=True))
+
+        elif args.template:
+            tplstruct = json.loads(
+                api_request("templates", {"template": args.template}))
+            if args.role:
+                rs = json.loads(
+                    api_request("query",
+                                {"fqdn": args.role,
+                                 "template": args.template}))
                 if args.mm:
                     pretty_print_audit(tplstruct, rs, args)
                 else:
-                    print( json.dumps(rs, indent=4, sort_keys=True))
-            elif args.envid :
-                rs =  json.loads(api_request( "query", { "domain" : args.envid, "template" : args.template } ))
-                if args.mm :
+                    print(json.dumps(rs, indent=4, sort_keys=True))
+            elif args.envid:
+                rs = json.loads(
+                    api_request("query",
+                                {"domain": args.envid,
+                                 "template": args.template}))
+                if args.mm:
                     pretty_print_audit(tplstruct, rs, args)
-                else : 
-                    print( json.dumps(rs, indent=4, sort_keys=True))
-                
+                else:
+                    print(json.dumps(rs, indent=4, sort_keys=True))
+
         sys.exit()
-        
+
     except KeyboardInterrupt:
         ### handle keyboard interrupt ###
         return 0
     except Exception, e:
-        if DEBUG :
+        if DEBUG:
             raise(e)
         indent = len(program_name) * " "
         sys.stderr.write(program_name + ": " + str(e) + "\n")
